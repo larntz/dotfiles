f69f37e0a5460658ee9b62cde0c576c475c081a7
Luke Arntz <luke.arntz@swimlane.com>
2021-12-29 17:45:55 -0500

    add weather to polyar

--- .config/polybar/weather.sh
+++ .config/polybar/weather.sh
@@ -8,13 +8,15 @@
 REPORT=$HOME/.weather
 LAST_REPORT_EPOCH=$(cat $REPORT | jq -r .location.localtime_epoch)
 NOW_EPOCH=$(date +%s)
+API_KEY=$(cat $HOME/.weather-api-key)
+ZIPCODE=$(cat $HOME/.weather-zipcode)
 
 # arithmetic in bash is awesome
 LAST_REPORT_SECONDS=$((NOW_EPOCH-LAST_REPORT_EPOCH))
 #echo "it's been $LAST_REPORT_SECONDS seconds since the last api call"
 
 if (( $LAST_REPORT_SECONDS > 1800 )); then
-  curl -s -o $HOME/.weather 'https://api.weatherapi.com/v1/current.json?key=b40c149d1c264db4b99210153212912&q=33418&aqi=no'
+  curl -s -o $HOME/.weather "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$ZIPCODE&aqi=no"
 fi
 
 CURRENT_TEMP=$(cat $REPORT | jq -r .current.feelslike_f)
