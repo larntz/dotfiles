ce40f286b4e8c678f376b391056f8be28c0b7032
Luke Arntz <luke.arntz@swimlane.com>
2022-01-07 17:49:04 -0500

    config updates

--- .config/polybar/weather.sh
+++ .config/polybar/weather.sh
@@ -29,21 +29,12 @@ get_icon() {
     echo $icon
 }
 
-## here we get new info if the last check was >= 1800 seconds ago
-#LAST_REPORT=$(cat $HOME/.weather)
 REPORT=$HOME/.weather
-LAST_REPORT_EPOCH=$(cat $REPORT | jq -r .location.localtime_epoch)
-NOW_EPOCH=$(date +%s)
 API_KEY=$(cat $HOME/.weather-api-key)
 ZIPCODE=$(cat $HOME/.weather-zipcode)
 
-# arithmetic in bash is awesome
-LAST_REPORT_SECONDS=$((NOW_EPOCH-LAST_REPORT_EPOCH))
-#echo "it's been $LAST_REPORT_SECONDS seconds since the last api call"
-
-if (( $LAST_REPORT_SECONDS > 60 )); then
-  curl -s -o $HOME/.weather "api.openweathermap.org/data/2.5/weather?zip=33418,us&units=imperial&appid=$API_KEY"
-fi
+# get the weather
+curl -s -o $HOME/.weather "api.openweathermap.org/data/2.5/weather?zip=33418,us&units=imperial&appid=$API_KEY"
 
 CURRENT_TEMP=$(printf "%.0f" $(cat $REPORT | jq -r .main.feels_like))
 ICON=$(cat $REPORT | jq -r '.weather[0].icon')
