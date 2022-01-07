c6331a01df2e4792ee1c3f2e438854f37d7ab6e4
Luke Arntz <luke.arntz@swimlane.com>
2022-01-07 17:42:12 -0500

    config updates

--- .config/polybar/weather.sh
+++ .config/polybar/weather.sh
@@ -2,6 +2,7 @@
 
 # script get the weather. meant to be used with polybar.
 # uses https://www.weatherapi.com
+# weather-icons https://erikflowers.github.io/weather-icons/
 
 get_icon() {
     case $1 in
@@ -23,22 +24,6 @@ get_icon() {
         50d) icon="";;
         50n) icon="";;
         *) icon="";
-
-        # Icons for Font Awesome 5 Pro
-        #01d) icon="";;
-        #01n) icon="";;
-        #02d) icon="";;
-        #02n) icon="";;
-        #03d) icon="";;
-        #03n) icon="";;
-        #04*) icon="";;
-        #09*) icon="";;
-        #10d) icon="";;
-        #10n) icon="";;
-        #11*) icon="";;
-        #13*) icon="";;
-        #50*) icon="";;
-        #*) icon="";
     esac
 
     echo $icon
@@ -57,8 +42,7 @@ LAST_REPORT_SECONDS=$((NOW_EPOCH-LAST_REPORT_EPOCH))
 #echo "it's been $LAST_REPORT_SECONDS seconds since the last api call"
 
 if (( $LAST_REPORT_SECONDS > 60 )); then
-  curl -s -o $HOME/.weather 'api.openweathermap.org/data/2.5/weather?zip=33418,us&units=imperial&appid=19fe0c149c6857855e6b1fc2ad7abc0b'
-  #curl -s -o $HOME/.weather "https://api.weatherapi.com/v1/current.json?key=$API_KEY&q=$ZIPCODE&aqi=no"
+  curl -s -o $HOME/.weather "api.openweathermap.org/data/2.5/weather?zip=33418,us&units=imperial&appid=$API_KEY"
 fi
 
 CURRENT_TEMP=$(printf "%.0f" $(cat $REPORT | jq -r .main.feels_like))
@@ -67,4 +51,3 @@ ICON=$(cat $REPORT | jq -r '.weather[0].icon')
 DEGREE=\\u00B0
 
 echo -e "$CURRENT_TEMP${DEGREE}f %{F#458588}%{T7}$(get_icon "$ICON")%{F-}%{T0}${CONDITION} "
-#printf "$CURRENT_TEMP${DEGREE}f"
