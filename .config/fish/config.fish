455677e69a76a829a361d61bfdaccd29128103f6
Luke Arntz <luke.arntz@swimlane.com>
2023-04-28 07:51:45 -0400

    fish, tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     export GOPATH=$HOME/.gopath
     export EDITOR=/usr/local/bin/vi
+    fish_vi_key_bindings
     starship init fish | source
 end
